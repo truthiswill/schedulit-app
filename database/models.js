2051f1d59ed0f32ec18efb2868a13677546a2475
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-18 20:19:19 -0700

    export models instead of schemas

--- database/models.js
+++ database/models.js
@@ -1,39 +1,43 @@
 const { mongoose } = require('./index');
+let timeSlotSchema = mongoose.Schema({
+  startTime: Date,
+  endTime: Date,
+  preferenceLevel: Number
+});
 
+let userSchema = mongoose.Schema({
+  id: { type: ObjectId, unique: true },
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  eventsCreated: [ObjectId],
+  eventsJoined: [ObjectId]
+});
+
+let participationSchema = mongoose.Schema({
+  id: ObjectId,
+  userId: ObjectId,
+  eventId: ObjectId,
+  timeAvailable: [timeSlotSchema],
+  preferences: {
+    type: Map,
+    of: String
+  }
+});
+
+let eventSchema = mongoose.Schema({
+  id: { type: ObjectId, unique: true },
+  creatorId: ObjectId,
+  title: String,
+  description: String,
+  availableSlots: [timeSlotSchema],
+  participants: [ObjectId],
+  allowedPreferences: [String]
+});
 module.exports = {
-  timeSlotSchema: mongoose.Schema({
-    startTime: Date,
-    endTime: Date,
-    preferenceLevel: Number
-  }),
-
-  userSchema: mongoose.Schema({
-    id: { type: ObjectId, unique: true },
-    firstName: String,
-    lastName: String,
-    email: { type: String, unique: true },
-    eventsCreated: [ObjectId],
-    eventsJoined: [ObjectId]
-  }),
-
-  participationSchema: mongoose.Schema({
-    id: ObjectId,
-    userId: ObjectId,
-    eventId: ObjectId,
-    timeAvailable: [timeSlotSchema],
-    preferences: {
-      type: Map,
-      of: String
-    }
-  }),
-
-  eventSchema: mongoose.Schema({
-    id: { type: ObjectId, unique: true },
-    creatorId: ObjectId,
-    title: String,
-    description: String,
-    availableSlots: [timeSlotSchema],
-    participants: [ObjectId],
-    allowedPreferences: [String]
-  });
+  timeSlot: mongoose.model('TimeSlot', timeSlotSchema),
+  user: mongoose.model('User', userSchema),
+  participation: mongoose.model('Participation', participationSchema),
+  event: mongoose.model('Event', eventSchema)
+};
 
