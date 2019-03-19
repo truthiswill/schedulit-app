90be08df1fb670254caf5e99672643d40cec3740
Andrew <andrewahnjr@gmail.com>
2019-03-19 10:13:55 -0700

    Hover over home and create

--- public/src/components/Navigation.jsx
+++ public/src/components/Navigation.jsx
@@ -4,8 +4,12 @@ import styles from '../styles/Navigation.css';
 const Navigation = ({ changeHomeView, changeCreateView }) => {
   return (
     <div className={styles.navigation}>
-      <div onClick={changeHomeView}>Home</div>
-      <div onClick={changeCreateView}>Create an event!</div>
+      <div className={styles.home} onClick={changeHomeView}>
+        Home
+      </div>
+      <div className={styles.create} onClick={changeCreateView}>
+        Create an event!
+      </div>
     </div>
   );
 };
