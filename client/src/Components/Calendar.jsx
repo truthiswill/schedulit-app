1499c254a508aaf343e82c27aa6229e79498eaff
Scott Nassau <snassau@gmail.com>
2019-03-20 10:03:45 -0700

    working on calendar, hunting bugs

---
+++ client/src/Components/Calendar.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import {format, startOfWeek, addDays, startOfMonth, endOfMonth, endOfWeek, isSameMonth, isSameDay, parse, subMonths, addMonths} from 'date-fns';
+import styles from '../../dist/styles/calendar.css';
+import classnames from 'classnames';
+
+class Calendar extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      currentMonth: new Date (),
+      selectedDate: new Date(),
+    }
+    this.renderHeader = this.renderHeader.bind(this);
+    this.renderDays = this.renderDays.bind(this);
+    this.renderCells = this.renderCells.bind(this);
+    this.onDateClick = this.onDateClick.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
+    this.prevMonth = this.prevMonth.bind(this);
+  }
+
+  renderHeader () {
+    const dateFormat = 'MMMM YYYY';
+    return (
+      <div className={classnames(styles.header, styles.row, styles.middle)}>
+        <div className={classnames(styles.col, styles.colStart)}>
+          <div className={styles.icon} onClick={this.prevMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className={classnames(styles.col, styles.colCenter)}>
+          <span> {format(this.state.currentMonth, dateFormat)} </span>
+        </div>
+        <div className={classnames(styles.col, styles.colEnd)} onClick={this.nextMonth}>
+          <div className={styles.icon}>chevron_right</div>
+        </div>
+      </div>
+    );  
+  }
+
+  renderDays () {
+    const dateFormat = 'dddd';
+    const days = [];
+
+    let startDate = startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push (
+        <div className={classnames(styles.col, styles.colCenter)} key={i}>
+          {format(addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    };
+    return <div className={classnames(styles.days, styles.row)}>{days}</div>
+  }
+
+  renderCells () {
+    const {currentMonth, selectedDate} = this.state;
+    const monthStart = startOfMonth(currentMonth);
+    const monthEnd = endOfMonth(monthStart);
+    const startDate = startOfWeek(monthStart);
+    const endDate = endOfWeek(monthEnd);
+
+    const dateFormat = 'D';
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = '';
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, dateFormat);
+        const cloneDay = day;
+        days.push (
+          <div 
+          // className={`col cell ${!isSameMonth(day, monthStart) ? "disabled" : isSameDay(day, selectedDate) ? "selected" : ""}`}
+            key={day}
+            onClick={() => this.onDateClick(parse(cloneDay))}>
+            <span className={styles.number}>{formattedDate}</span>
+            <span className={styles.bg}>{formattedDate}</span>
+          </div>
+        );
+        day = addDays(day, 1);
+      }
+      rows.push (
+        <div className={styles.row} key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className={styles.body}>{rows}</div>
+  };
+
+  onDateClick (day) {
+    this.setState({
+      selectedDate: day
+    })
+  }
+
+  nextMonth () {
+    this.setState ({
+      currentMonth: addMonths(this.state.currentMonth, 1)
+    })
+  };
+
+  prevMonth () {
+    this.setState ({
+      currentMonth: subMonths(this.state.currentMonth, 1)
+    })
+  };
+
+  render () {
+    return (
+      <div className={styles.calendar}>
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  };
+}
+
+export default Calendar;
--- client/src/Components/Create.jsx
+++ client/src/Components/Create.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import styles from '../../dist/styles/create.css';
+import Calendar from './Calendar.jsx'
+import { getDaysInMonth, getMonth, getYear } from 'date-fns';
 
 class Create extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+
+    this.state = { 
+      value: '',
+      // today: new Date(),
+      // year: getYear(this.state.today),
+      // month: getMonth(this.state.today),
+      months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.previousMonth = this.previousMonth.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
+    this.renderMonth = this.renderMonth(this);
   }
+
+  // componentDidMount() {
+  //   console.log (this.state.today)
+  //   console.log (getDaysInMonth(this.state.today), 'days this month');
+  //   console.log (this.state.months[getMonth(this.state.today)], 'this month');
+  //   console.log (getYear(this.state.today), 'year')
+  // }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
@@ -18,21 +38,64 @@ class Create extends Component {
     event.preventDefault();
   }
 
+  previousMonth() {
+
+  }
+
+  nextMonth () {
+
+  }
+
+  renderMonth (month, year) {
+    let firstDay = new Date(this.state.year, this.state.month)
+  }
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Event Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Event Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <Calendar/>
+      </div>
     );
   }
 }
 
 export default Create;
+
+
+{/* <div className={styles.calendarContainer}>
+          <div className={styles.calendar}>
+          <h3 className={styles.calendarHeader}></h3>
+          <table className={styles.calendarBorder}>
+            <thead>
+            <tr>
+              <th>Sun</th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+            </tr>
+            </thead>
+            <tbody className={styles.calendarBody}>
+            </tbody>
+          </table>
+
+          <div className={styles.calendarButtons}>
+            <button className={styles.previousMonth} onClick={this.previousMonth()}>Previous</button>
+            <button className={styles.nextMonth} onClick={this.nextMonth()}>Next</button>
+          </div>
+          <br/>
+          </div> 
+        </div> */}
