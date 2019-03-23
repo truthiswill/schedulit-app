50761494e4ff1dd4c3484a0fa18d42c2b6384db0
Bryant Lin <br4qua@gmail.com>
2019-03-23 14:22:56 -0700

    Add styling to front page and login page

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -8,7 +8,6 @@ class Login extends Component {
 
   render() {
     return (
-
       <div className={`${styles.overAll}`}>
         <div className={`${styles.inner}`}>
           <div className={`${styles.directions}`}>
