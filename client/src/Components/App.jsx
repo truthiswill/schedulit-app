d90f0ce8d317c2119073886cbd93ea13cc942fe8
Bryant Lin <br4qua@gmail.com>
2019-03-23 16:47:10 -0700

    fix page flow

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -16,13 +16,13 @@ class App extends Component {
       view: 'loginPage',
       events: []
     };
-    let eventId = Cookies.get('eventId');
-    this.joinEventIfExists(eventId);
-
+    
+		this.fetchEvents = this.fetchEvents.bind(this);
+		this.fetchEvents();
     this.setEventView = this.setEventView.bind(this);
     this.setCreateView = this.setCreateView.bind(this);
     this.loginUser = this.loginUser.bind(this);
-    
+    this.joinEventIfExists = this.joinEventIfExists.bind(this)
     window.isUserLoggedIn = false;
     window.forceReactUpdate = this.forceUpdate.bind(this);
   }
@@ -41,35 +41,35 @@ class App extends Component {
               // not including preference level as not meaningful
             };
           });
-          this.setState({ eventData: data }, () => {
-            this.fetchEvents();
-          });
+          this.setState({ eventData: data, view: 'joinPage' });
         })
         .catch(() => console.log('event does not exist'));
     }
   }
 
   componentDidMount() {
-    this.fetchEvents();
+		let eventId = Cookies.get('eventId');
+    this.joinEventIfExists(eventId);
   }
 
-  fetchEvents() {
-    axios
+   fetchEvents() {
+     axios
       .get('/api/user')
       .then(({ data }) => {
         let events = data.eventsCreated;
 
         //Checks if user is logged in
         if (data.id !== undefined) {
-          this.setState({ loggedIn: true });
-        }
+					this.setState({ loggedIn: true, view: "eventPage"});
+        } 
 
         Promise.all(
           events.map(event => {
             return axios.get('/api/event/' + event).then(({ data }) => data);
           })
         ).then(eventsArr => {
-          this.setState({ events: eventsArr });
+					this.setState({ events: eventsArr});
+					
         });
       })
       .catch(error => console.error(error));
@@ -91,22 +91,26 @@ class App extends Component {
   render() {
 
 		let display;
-
+		console.log('this.state.loggedin', this.state.loggedIn)
+		console.log('this.state.view', this.state.view)
 		if (this.state.loggedIn) {
       if (this.state.view === 'createPage') {
 				display = <Create />;
       } else if (this.state.view === 'eventPage') {
-        display = <Events events={this.state.events} />;
-      } else if (this.state.eventData !== undefined) {
+				display = <Events events={this.state.events} joinEventIfExists = {this.joinEventIfExists}/>;
+				console.log(this.state. eventData)
+      } else if (this.state.eventData !== undefined || this.state.view ==='joinPage') {
 				display = <JoinEvent eventData={this.state.eventData} />;
 			}
     } else {
       display = <Login loginUser={this.loginUser} />;
     }
 	
-		console.log(window.isUserLoggedIn);
+
     if (window.isUserLoggedIn) {
-      this.fetchEvents();
+			console.log('triggered forceupdate from popup')
+			this.fetchEvents();
+			this.setState({loggedIn: true, view:'eventPage'})
       window.isUserLoggedIn = false; // sorry hacky but less line
       display = <Events events={this.state.events} />;
     }
