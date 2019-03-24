bf499a47bc7181ebfb3087da5452282f3ea1d958
Andrew <andrewahnjr@gmail.com>
2019-03-24 11:58:40 -0700

    Add copy link feature

--- client/src/Components/Event.jsx
+++ client/src/Components/Event.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Participants from './Participants.jsx';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styles from '../styles/event.css';
-
-/* <a href={'/join/' + props.event.id}>{props.event.title}</a> */
 
 class Event extends Component {
   constructor(props) {
     super(props);
+  }
+
+  getLink(eventId) {
+    let link = `http://localhost:3000/join/${eventId}`;
+    return link;
   }
 
   render() {
@@ -19,6 +23,9 @@ class Event extends Component {
           {this.props.event.title}
         </div>
         <div>{this.props.event.description}</div>
+        <CopyToClipboard text={this.getLink(this.props.event.id)}>
+          <button>Copy Link</button>
+        </CopyToClipboard>
         <Participants participants={this.props.event.participants} />
       </div>
     );
